9e885ee27b3cab0c2be7c27b3ce4e7feaff801e2
Javis V. Pérez <javisperez@gmail.com>
2026-02-13 21:59:42 -0400

    Enable github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.BASE_URL || '/',
   plugins: [tailwindcss(), react()],
   resolve: {
     alias: {
